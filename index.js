ceee77ae9deb7a627281295078d134fef12407df
Timothy Barasa <timbaros87@gmail.com>
2024-04-08 18:33:51 +0300

    add text to the new header

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 // Write your code here!
-
+// Remove main using the ID 
 document.getElementById('main').remove();
 
+// Create a new header variable that points to h1.
 const newHeader = document.createElement('h1');
+
+// Assign the id "victory" to the new header.
 newHeader.id = 'victory';
+
+// Assign text to the new header variable.
 newHeader.innerHTML = "TIM-BAROS is the champion";
 
 
